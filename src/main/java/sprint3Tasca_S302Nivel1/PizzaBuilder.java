b0977055fdc185bc02eb21d37a3d5516b330641f
jrffruit518-enrich <jrffruit518@gmail.com>
2025-11-16 15:54:29 +0100

    feat: add PizzaBuilder.java fix: Pizza.yuml

---
+++ src/main/java/sprint3Tasca_S302Nivel1/PizzaBuilder.java
@@ -0,0 +1,17 @@
+package sprint3Tasca_S302Nivel1;
+
+/**
+ * ClassName: PizzaBuilder
+ * Package: sprint3Tasca_S302Nivel1
+ * Description:
+ * Author: Rong Jiang
+ * Create:16/11/2025 - 15:51
+ * Version:v1.0
+ */
+public interface PizzaBuilder {
+    public PizzaBuilder buildSize(int size);
+    public PizzaBuilder buildDough(String dough);
+    public PizzaBuilder addTopping(String tipping);
+    public Pizza getResult();
+
+}
