e3db9e3148c35b10dda8c3c000c9231d04bb495d
jrffruit518-enrich <jrffruit518@gmail.com>
2025-11-17 21:18:17 +0100

    feat: initial commit

---
+++ src/main/java/sprint3Tasca_S302Nivel2/observer/Observer.java
@@ -0,0 +1,15 @@
+package sprint3Tasca_S302Nivel2.observer;
+
+import sprint3Tasca_S302Nivel2.StockState;
+
+/**
+ * ClassName: Observer
+ * Package: sprint3Tasca_S302Nivel2.observer
+ * Description:
+ * Author: Rong Jiang
+ * Create:17/11/2025 - 21:01
+ * Version:v1.0
+ */
+public interface Observer {
+ public void update(StockState stockState);
+}
